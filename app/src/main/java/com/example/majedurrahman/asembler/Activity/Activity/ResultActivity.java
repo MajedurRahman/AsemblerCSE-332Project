a5d161ce9032d7fe79587ef1d22c74693e559fdd
Majedur Rahman <Majedur.Rahman.mmr@gmail.com>
2017-07-11 02:09:34 +0600

    My CSE332-Project asember updated .......

--- app/src/main/java/com/example/majedurrahman/asembler/Activity/Activity/ResultActivity.java
+++ app/src/main/java/com/example/majedurrahman/asembler/Activity/Activity/ResultActivity.java
@@ -1,24 +1,23 @@
 package com.example.majedurrahman.asembler.Activity.Activity;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.example.majedurrahman.asembler.Activity.FileWriter;
 import com.example.majedurrahman.asembler.R;
 
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.File;
 
 
 public class ResultActivity extends AppCompatActivity {
 
 
-    TextView textView2,textView1;
+    TextView textView2, textView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,51 +25,31 @@ public class ResultActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         String Code = bundle.getString("Code");
-        String ins = bundle.getString("Assembly");
+        String instraction = bundle.getString("Assembly");
         textView1 = (TextView) findViewById(R.id.instTv);
         textView2 = (TextView) findViewById(R.id.resultTv);
 
         textView2.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
         textView1.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
-        textView1.setText(ins.toString());
+        textView1.setText(instraction.toString());
         textView2.setText(Code.toString());
 
-        writeToFile(Code,this);
-        getDataFromFile();
-        writeIntoFile(Code);
+        writeIntoFile(Code , instraction);
 
 
     }
 
-    private void writeIntoFile(String data) {
+    private void writeIntoFile(String code , String instruction) {
+        long nameExtendedKey = SystemClock.elapsedRealtime();
+        FileWriter fileWriter = new FileWriter(ResultActivity.this);
 
-        SharedPreferences  shp = getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor= shp.edit();
-        editor.putString("Code",data);
-
-        editor.apply();
-    }
-
-    private String getDataFromFile() {
-
-        SharedPreferences sh = getPreferences(Context.MODE_PRIVATE);
-
-       String data = sh.getString("Code","Data Not Found !! ");
-
-        return data;
-    }
-
-
-    private void writeToFile(String data,Context context) {
-
-    try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("CODE.txt", Context.MODE_PRIVATE));
-            outputStreamWriter.write(data);
-            outputStreamWriter.close();
-            Toast.makeText(context, "Write Data Finished !! ", Toast.LENGTH_SHORT).show();
+        try {
+            File subDirectory = fileWriter.createSubDirectory(fileWriter.getExternalStorageDirectory(), "Assembler Folder");
+            fileWriter.writeDataToFile(subDirectory, nameExtendedKey+"_Code.txt", code.toString());
+            fileWriter.writeDataToFile(subDirectory, nameExtendedKey+"_Instructions.txt", instruction.toString());
+        } catch (FileWriter.ExternalFileWriterException e) {
+            e.printStackTrace();
         }
-        catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
+
     }
 }
