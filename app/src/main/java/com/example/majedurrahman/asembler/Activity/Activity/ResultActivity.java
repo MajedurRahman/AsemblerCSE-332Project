ee76c5f19fd659d560c255ff97839c59711e199d
Majedur Rahman <Majedur.Rahman.mmr@gmail.com>
2017-07-11 18:03:18 +0600

    10 Bit assembler

--- app/src/main/java/com/example/majedurrahman/asembler/Activity/Activity/ResultActivity.java
+++ app/src/main/java/com/example/majedurrahman/asembler/Activity/Activity/ResultActivity.java
@@ -3,6 +3,7 @@ package com.example.majedurrahman.asembler.Activity.Activity;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
 
@@ -25,17 +26,50 @@ public class ResultActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         String Code = bundle.getString("Code");
         String instraction = bundle.getString("Assembly");
+
+        String[] split = Code.split("\n");
+
+
         textView1 = (TextView) findViewById(R.id.instTv);
         textView2 = (TextView) findViewById(R.id.resultTv);
 
         textView2.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
         textView1.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);
+
         textView1.setText(instraction.toString());
         textView2.setText(Code.toString());
-
         writeIntoFile(Code, instraction);
+        getHexCode(split);
 
 
+    }
+
+    private void getHexCode(String[] split) {
+
+
+        for (int i = 0; i < split.length; i++) {
+
+            Log.e("hex", Integer.toHexString(Integer.parseInt(split[i])) + "");
+
+
+            //    Log.e("bbbbbbbbbb" , codsplit.length +"" + codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+codsplit[1]+);
+            //      Log.e("bbbb" , split.length+"  si  "+split[0]+split[1]+split[2]+split[3]+split[4]+split[5]+split[6]+split[7]+split[8]+split[9]+"");
+
+        }
+        /*for (int i = 0; i < splitt.length; i++) {
+
+            char[] split = splitt[i].toCharArray();
+
+            String h1 = String.valueOf(split[0] + split[1] + split[2] + split[3]);
+            String h2 = String.valueOf(split[4] + split[5] + split[6] + split[7]);
+            String h3 = String.valueOf(split[8] + split[9]);
+            // Log.e("HX " , h1 + "  " + h2 +" " + h3);
+
+            Log.e("HX ", "" + splitt[i]);
+            String HexValue = Integer.toHexString(Integer.parseInt(h3)) + Integer.toHexString(Integer.parseInt(h2)) + Integer.toHexString(Integer.parseInt(h1));
+            Log.e("HX ", HexValue);
+
+        }*/
     }
 
     // Write Instruction and Machine code if file
